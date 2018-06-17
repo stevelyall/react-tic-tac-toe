37eb461c00ced28bf08ff54a020d3689b43fa0d6
Steve Lyall <stevenlyall@gmail.com>
2018-06-17 11:22:56 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Game from "./game/Game";
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <Game></Game>
+    );
+  }
+}
+
+export default App;
---
+++ src/square/Square.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import './Square.css';
+
+export class Square extends Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
