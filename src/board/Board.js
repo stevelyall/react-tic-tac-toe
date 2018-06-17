6626daf43d12cd7e87303e51ec3a92aeef24d516
Steve Lyall <stevenlyall@gmail.com>
2018-06-17 11:57:02 -0700

    working game

--- src/board/Board.js
+++ src/board/Board.js
@@ -2,12 +2,32 @@ import React, {Component} from "react";
 import {Square} from '../square/Square';
 
 export class Board extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+
+        }
+    }
+
     renderSquare(i) {
-        return <Square/>;
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />;
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = this.calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        }
+        else {
+            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        }
 
         return (
             <div>
@@ -30,4 +50,39 @@ export class Board extends Component {
             </div>
         );
     }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+
+        if (this.calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
 }
--- src/square/Square.js
+++ src/square/Square.js
@@ -1,12 +1,10 @@
-import React, {Component} from "react";
+import React from "react";
 import './Square.css';
 
-export class Square extends Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
-    }
+export function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
